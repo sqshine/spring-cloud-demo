e11a735061ac6d63285689937a0276149326622c
shiquan <shiquan@flybycloud.com>
2016-11-30 16:26:21 +0800

    注册中心配置

--- registry-center-cloud/src/main/java/com/sqshine/RegistryCenterCloudApplication.java
+++ registry-center-cloud/src/main/java/com/sqshine/RegistryCenterCloudApplication.java
@@ -2,11 +2,13 @@ package com.sqshine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class RegistryCenterCloudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RegistryCenterCloudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryCenterCloudApplication.class, args);
+    }
 }
