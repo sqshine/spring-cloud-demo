f6367a74af04c9bc103f391cac18b9fbf025996e
shiquan <shiquan@flybycloud.com>
2016-11-30 16:56:58 +0800

    服务提供方

---
+++ service-cloud/src/main/java/com/sqshine/ServiceCloudApplication.java
@@ -0,0 +1,14 @@
+package com.sqshine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServiceCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCloudApplication.class, args);
+	}
+}
