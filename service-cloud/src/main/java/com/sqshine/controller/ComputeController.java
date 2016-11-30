9220b8dcf930867b2f7e7670328e8064d143a5cc
shiquan <shiquan@flybycloud.com>
2016-11-30 17:02:01 +0800

    服务提供方修改

--- service-cloud/src/main/java/com/sqshine/controller/ComputeController.java
+++ service-cloud/src/main/java/com/sqshine/controller/ComputeController.java
@@ -1,9 +1,5 @@
 package com.sqshine.controller;
 
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,16 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ComputeController {
-    private final Logger logger = Logger.getLogger(getClass());
-
-    @Autowired
-    private DiscoveryClient client;
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
-        ServiceInstance instance = client.getLocalServiceInstance();
         Integer r = a + b;
-        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
         return r;
     }
 
