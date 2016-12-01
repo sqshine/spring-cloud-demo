21f5fb684af6dc3b8f537f1a115825b4b4ff9b33
shiquan <shiquan@flybycloud.com>
2016-12-01 16:32:00 +0800

    添加读取配置中心配置的客户端

---
+++ config-client/src/main/java/com/sqshine/ConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.sqshine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+}
