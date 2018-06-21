ac93202c173b7f876eff5af7a70490679b1b1a0f
sqshine <>
2018-06-21 18:03:49 +0800

    update user

--- user-provider/src/main/java/com/sqshine/domain/User.java
+++ user-provider/src/main/java/com/sqshine/domain/User.java
@@ -1,26 +1,18 @@
 package com.sqshine.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
+/**
+ * @author sqshine
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
-
-    public User(Long id, String username) {
-        super();
-        this.id = id;
-        this.username = username;
-    }
-
-    public User(Long id, String username, String name, Integer age, BigDecimal balance) {
-        this.id = id;
-        this.username = username;
-        this.name = name;
-        this.age = age;
-        this.balance = balance;
-    }
-
-    public User() {
-        super();
-    }
 
     private Long id;
 
@@ -31,44 +23,4 @@ public class User {
     private Integer age;
 
     private BigDecimal balance;
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return this.username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return this.age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public BigDecimal getBalance() {
-        return this.balance;
-    }
-
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }
 }
