8deb28b86ee88b3c8cb1a6012b67fa1dd6db4175
sqshine <>
2018-06-21 13:02:21 +0800

    update to spring cloud Finchley and spring boot 2.0.3

--- service-cloud/src/main/java/com/sqshine/controller/ComputeController.java
+++ service-cloud/src/main/java/com/sqshine/controller/ComputeController.java
@@ -1,16 +1,23 @@
 package com.sqshine.controller;
 
-import org.apache.log4j.Logger;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+/**
+ * @author sqshine
+ */
 @RestController
+@Slf4j
 public class ComputeController {
-
-    private final Logger logger = Logger.getLogger(getClass());
 
     @Autowired
     private DiscoveryClient discoveryClient;
@@ -24,12 +31,16 @@ public class ComputeController {
     }
 
     @GetMapping(value = "/add")
-    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+    public Integer add(@RequestParam("a") Integer a, @RequestParam("b") Integer b) {
         String serviceId = registration.getServiceId();
-        ServiceInstance serviceInstance = discoveryClient.getLocalServiceInstance();
-        Integer r = a + b;
-        logger.info("/add, host:{}" + serviceInstance.getHost() + ", service_id:" + serviceInstance.getServiceId() + ", result:" + r);
-        return r;
+        List<ServiceInstance> list = discoveryClient.getInstances(serviceId);
+        Integer result = a + b;
+        if (list != null && list.size() > 0) {
+            ServiceInstance serviceInstance = list.get(0);
+            log.info("/add, host:{}" + serviceInstance.getHost() + ", service_id:" + serviceInstance.getServiceId() + ", result:" + result);
+        }
+
+        return result;
     }
 
 }
