eb458fa17e924c60c127f67a4e035357337febc7
sqshine <>
2018-06-21 18:03:35 +0800

    update user

--- movie-consumer/src/main/java/com/sqshine/domain/User.java
+++ movie-consumer/src/main/java/com/sqshine/domain/User.java
@@ -1,7 +1,10 @@
 package com.sqshine.domain;
 
+import lombok.Data;
+
 import java.math.BigDecimal;
 
+@Data
 public class User {
 
     private Long id;
@@ -14,43 +17,4 @@ public class User {
 
     private BigDecimal balance;
 
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return this.username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return this.age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public BigDecimal getBalance() {
-        return this.balance;
-    }
-
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }
 }
