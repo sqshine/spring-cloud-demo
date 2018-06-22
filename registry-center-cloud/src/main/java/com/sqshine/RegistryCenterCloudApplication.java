7cfce3922c6ee2228cc486a7aca7fc155b061ece
sqshine <>
2018-06-22 08:29:04 +0800

    update

--- registry-center-cloud/src/main/java/com/sqshine/RegistryCenterCloudApplication.java
+++ registry-center-cloud/src/main/java/com/sqshine/RegistryCenterCloudApplication.java
@@ -1,11 +1,16 @@
 package com.sqshine;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author sqshine
+ */
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAdminServer
 public class RegistryCenterCloudApplication {
 
     public static void main(String[] args) {
