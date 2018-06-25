63b846ed931fd30f3527df0e2a1a939a660c0aab
sqshine <>
2018-06-25 18:00:51 +0800

    spring cloud gateway work

--- api-gateway/src/main/java/com/sqshine/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/sqshine/gateway/ApiGatewayApplication.java
@@ -2,6 +2,9 @@ package com.sqshine.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.CoreSubscriber;
@@ -11,15 +14,19 @@ import reactor.core.publisher.Mono;
  * @author sqshine
  */
 @SpringBootApplication
-@RestController
 public class ApiGatewayApplication {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                //basic proxy
+                .route(r -> r.path("/baidu")
+                        .uri("http://baidu.com:80/")
+                ).build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public Mono<String> hello() {
-        return null;
-    }
 }
