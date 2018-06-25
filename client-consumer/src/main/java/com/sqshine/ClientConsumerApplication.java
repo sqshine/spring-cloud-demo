953790c512641ec88b0f81519aa94ced731bfbdc
sqshine <>
2018-06-25 14:04:16 +0800

    update

--- client-consumer/src/main/java/com/sqshine/ClientConsumerApplication.java
+++ client-consumer/src/main/java/com/sqshine/ClientConsumerApplication.java
@@ -1,26 +1,31 @@
 package com.sqshine;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * @author sqshine
  */
-@SpringBootApplication
-//@EnableDiscoveryClient
+@SpringCloudApplication
 @EnableFeignClients
-@EnableCircuitBreaker
 public class ClientConsumerApplication {
+
+    @Bean
+    @ConfigurationProperties(prefix = "custom.rest.connection")
+    public HttpComponentsClientHttpRequestFactory httpRequestFactory() {
+        return new HttpComponentsClientHttpRequestFactory();
+    }
 
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return new RestTemplate(httpRequestFactory());
     }
 
     public static void main(String[] args) {
